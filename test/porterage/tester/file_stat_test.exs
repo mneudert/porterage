cac20e110a18fd9ba66e7395e5e00bb07edfcbf4
Marc Neudert <marc.neudert@gmail.com>
2020-01-17 19:46:08 +0100

    Extend tests for file stat tester

--- test/porterage/tester/file_stat_test.exs
+++ test/porterage/tester/file_stat_test.exs
@@ -4,6 +4,7 @@ defmodule Porterage.Scheduler.FileStatTest do
   alias Porterage.Scheduler.Once
   alias Porterage.Tester.FileStat
   alias Porterage.TestHelpers.DummyFetcher
+  alias Porterage.TestHelpers.DummyScheduler
 
   test "Initial startup triggers :fetch" do
     start_supervised(
@@ -18,5 +19,26 @@ defmodule Porterage.Scheduler.FileStatTest do
     )
 
     assert_receive :fetch
+  end
+
+  test "Unchanged file only triggers :fetch once" do
+    {:ok, sup_pid} =
+      start_supervised(
+        {Porterage,
+         %{
+           scheduler: DummyScheduler,
+           scheduler_opts: %{return_tick: false},
+           tester: FileStat,
+           tester_opts: %{file: Path.join([__DIR__, "file_stat_test.exs"]), stat_key: :mtime},
+           fetcher: DummyFetcher,
+           fetcher_opts: %{parent: self(), send_fetch: :fetch}
+         }}
+      )
+
+    Porterage.test(sup_pid)
+    Porterage.test(sup_pid)
+
+    assert_receive :fetch
+    refute_receive _, 100
   end
 end
