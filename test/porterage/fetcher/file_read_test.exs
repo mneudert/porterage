8d069763929859a3fda4bf8f0d05cc22459143db
Marc Neudert <marc.neudert@gmail.com>
2020-01-24 16:41:46 +0100

    Minimize configuration for fetcher tests

--- test/porterage/fetcher/file_read_test.exs
+++ test/porterage/fetcher/file_read_test.exs
@@ -3,46 +3,42 @@ defmodule Porterage.Fetcher.FileReadTest do
 
   alias Porterage.Fetcher.FileRead
   alias Porterage.TestHelpers.DummyDeliverer
-  alias Porterage.TestHelpers.DummyScheduler
-  alias Porterage.TestHelpers.DummyTester
 
   test "file is read and send to deliverer" do
-    start_supervised(
-      {Porterage,
-       %{
-         deliverer: DummyDeliverer,
-         deliverer_opts: %{parent: self()},
-         fetcher: FileRead,
-         fetcher_opts: %{
-           file: Path.join(__DIR__, "file_read_test.exs")
-         },
-         scheduler: DummyScheduler,
-         scheduler_opts: %{return_tick: true},
-         tester: DummyTester,
-         tester_opts: %{return_test: true}
-       }}
-    )
+    {:ok, sup_pid} =
+      start_supervised(
+        {Porterage,
+         %{
+           deliverer: DummyDeliverer,
+           deliverer_opts: %{parent: self()},
+           fetcher: FileRead,
+           fetcher_opts: %{
+             file: Path.join(__DIR__, "file_read_test.exs")
+           }
+         }}
+      )
+
+    Porterage.fetch(sup_pid)
 
     assert_receive contents
     assert contents =~ "defmodule Porterage.Fetcher.FileReadTest"
   end
 
   test "missing files are ignored" do
-    start_supervised(
-      {Porterage,
-       %{
-         deliverer: DummyDeliverer,
-         deliverer_opts: %{parent: self()},
-         fetcher: FileRead,
-         fetcher_opts: %{
-           file: Path.join(__DIR__, "file-that-does-not-exist")
-         },
-         scheduler: DummyScheduler,
-         scheduler_opts: %{return_tick: true},
-         tester: DummyTester,
-         tester_opts: %{return_test: true}
-       }}
-    )
+    {:ok, sup_pid} =
+      start_supervised(
+        {Porterage,
+         %{
+           deliverer: DummyDeliverer,
+           deliverer_opts: %{parent: self()},
+           fetcher: FileRead,
+           fetcher_opts: %{
+             file: Path.join(__DIR__, "file-that-does-not-exist")
+           }
+         }}
+      )
+
+    Porterage.fetch(sup_pid)
 
     refute_receive _, 100
   end
--- test/porterage/fetcher/mfa_test.exs
+++ test/porterage/fetcher/mfa_test.exs
@@ -3,27 +3,24 @@ defmodule Porterage.Fetcher.MFATest do
 
   alias Porterage.Fetcher.MFA
   alias Porterage.TestHelpers.DummyDeliverer
-  alias Porterage.TestHelpers.DummyScheduler
-  alias Porterage.TestHelpers.DummyTester
 
   test "fetcher can send data to deliverer" do
     defmodule MFADataFetcher do
       def fetch(data), do: {:ok, data}
     end
 
-    start_supervised(
-      {Porterage,
-       %{
-         deliverer: DummyDeliverer,
-         deliverer_opts: %{parent: self()},
-         fetcher: MFA,
-         fetcher_opts: %{mfa: {MFADataFetcher, :fetch, [:some_data]}},
-         scheduler: DummyScheduler,
-         scheduler_opts: %{return_tick: true},
-         tester: DummyTester,
-         tester_opts: %{return_test: true}
-       }}
-    )
+    {:ok, sup_pid} =
+      start_supervised(
+        {Porterage,
+         %{
+           deliverer: DummyDeliverer,
+           deliverer_opts: %{parent: self()},
+           fetcher: MFA,
+           fetcher_opts: %{mfa: {MFADataFetcher, :fetch, [:some_data]}}
+         }}
+      )
+
+    Porterage.fetch(sup_pid)
 
     assert_receive :some_data
   end
@@ -33,19 +30,18 @@ defmodule Porterage.Fetcher.MFATest do
       def fetch, do: :nodata
     end
 
-    start_supervised(
-      {Porterage,
-       %{
-         deliverer: DummyDeliverer,
-         deliverer_opts: %{parent: self()},
-         fetcher: MFA,
-         fetcher_opts: %{mfa: {MFANodataFetcher, :fetch, []}},
-         scheduler: DummyScheduler,
-         scheduler_opts: %{return_tick: true},
-         tester: DummyTester,
-         tester_opts: %{return_test: true}
-       }}
-    )
+    {:ok, sup_pid} =
+      start_supervised(
+        {Porterage,
+         %{
+           deliverer: DummyDeliverer,
+           deliverer_opts: %{parent: self()},
+           fetcher: MFA,
+           fetcher_opts: %{mfa: {MFANodataFetcher, :fetch, []}}
+         }}
+      )
+
+    Porterage.fetch(sup_pid)
 
     refute_receive _, 100
   end
