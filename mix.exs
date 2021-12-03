389f6e1fb9c3d24771f583e805b121d1b61d2ae8
Marc Neudert <marc.neudert@gmail.com>
2021-12-03 17:32:30 +0100

    Use SPDX license identifier

--- mix.exs
+++ mix.exs
@@ -84,7 +84,7 @@ defmodule Porterage.MixProject do
   defp package do
     %{
       files: ["CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{"GitHub" => @url_github}
     }
   end
