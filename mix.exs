142a2813825718b6850854d318579a854b04ab51
Marc Neudert <marc.neudert@gmail.com>
2023-04-14 13:12:42 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -43,8 +43,8 @@ defmodule Porterage.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.6", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.3", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.16.0", only: :test, runtime: false}
     ]
