f0798602d2b438c47763685b065cc3bc1af0ad89
Marc Neudert <marc.neudert@gmail.com>
2026-04-01 15:05:34 +0200

    Remove usage of deprecated :preferred_cli_env config

--- mix.exs
+++ mix.exs
@@ -17,16 +17,21 @@ defmodule Porterage.MixProject do
       docs: docs(),
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test
-      ],
       test_coverage: [tool: ExCoveralls]
     ]
   end
 
   def application do
     [extra_applications: [:logger]]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test
+      ]
+    ]
   end
 
   defp dialyzer do
